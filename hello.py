1eaba96e9c29e83abc74c23bd69eeab6cb4b45f6
Chaimae el marrouni <chaimae.elmarrouni@etu.uae.ac.ma>
2026-05-19 08:48:03 +0000

    mon premier job

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("=" * 40)
+print("Bienvenue dans mon premier job jenkins !")
+print("=" * 40)
+
+nom=input("quel est ton nom ? ")
+print(f"Bonjour{nom}, ton job jenkins a reussi !")
+
+a = 10
+b = 5
+print(f"{a} + {b} = {a +b}")
+print(f"{a} - {b} = {a -b}")
