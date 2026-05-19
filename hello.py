bc491af840fc2572a7a9849e42abebfbf14dc75d
Chaimae el marrouni <chaimae.elmarrouni@etu.uae.ac.ma>
2026-05-19 10:17:04 +0000

    Passage aux arguments sys.argv

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
+import sys
 print("=" * 40)
 print("Bienvenue dans mon premier job jenkins !")
 print("=" * 40)
 
-nom=input("quel est ton nom ? ")
+if len(sys.argv) > 1:
+   nom = sys.argv[1]
+else:
+   nom = "Etudiant Jenkins"
+
 print(f"Bonjour{nom}, ton job jenkins a reussi !")
 
 a = 10
 b = 5
 print(f"{a} + {b} = {a +b}")
 print(f"{a} - {b} = {a -b}")
+
+assert a +b == 15, "Le test a echoue !"
+print(" Tous les tests passent avec succes")
